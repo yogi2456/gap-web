d47951431a75939d1d1251196a4e326917b074ab
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-11-21 23:56:09 +0530

    updates

--- gapshop/src/Components/Navbar.js
+++ gapshop/src/Components/Navbar.js
@@ -7,12 +7,46 @@ function Navbar() {
     <div className='navbar'>
         <div className='navbar1'>
             <div className='navbar2'>
-                <div className='navbar3'></div>
-                <div className='navbar4'></div>
-                <div className='navbar5'></div>
+                <div className='navbar3'>
+                  <p>GAP</p>
+                  <p>OLD NAVY</p>
+                  <p>BANANA REPUBLIC</p>
+                  <p>ATHLETA</p>
+                  <p>babyGap</p>
+                </div>
+                <div className='navbar4'>
+                  <p>Free Shipping on $50+ for Rewards Members SIGN IN OR JOIN DETAILS</p>
+                </div>
+                <div className='navbar5'>
+                  <div>
+                    <p>Sign in</p>
+                    <p>Your Account</p>
+                  </div>
+                  <div></div>
+                  <div>
+                  <i class="fa-solid fa-bag-shopping" style={{color: "#e5e7eb"}}></i>
+                  </div>
+                </div>
             </div>
         </div>
-        <div></div>
+        <div className='navbar6'>
+          <div className='navbar7'>
+            <div className='navbar8'>
+              <p>GAP</p>
+              <p>NEW</p>
+              <p>WOMEN</p>
+              <p>MEN</p>
+              <p>GIRLS</p>
+              <p>BOYS</p>
+              <p>TODDLER</p>
+              <p>BABY</p>
+              <p>MATERNITY</p>
+              <p>SALE</p>
+              <p>THE GIFT SHOP</p>
+            </div>
+            <div className='navbar9'></div>
+          </div>
+        </div>
     </div>
     </>
   )
