4844e199f6ccd25f3d65944bd35d8f8cc500f8f0
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-11-21 19:46:42 +0530

    updates

--- gapshop/src/Components/Navbar.js
+++ gapshop/src/Components/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>
-    hello yogesh are you fine.
+    <>
+    <div className='navbar'>
+        <div className='navbar1'>
+            <div className='navbar2'>
+                <div className='navbar3'></div>
+                <div className='navbar4'></div>
+                <div className='navbar5'></div>
+            </div>
+        </div>
+        <div></div>
     </div>
+    </>
   )
 }
 
